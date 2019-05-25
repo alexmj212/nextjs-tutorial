4ba6927b29b72e8afff4264ff4b6802f7431a9bb
Alex Johnson <alexmj212@gmail.com>
2019-05-24 23:30:13 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+
+const PostLink = props => (
+    <li>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
+
+export default function Blog() {
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink id="hello-nextjs" title="Hello Next.js" />
+            </ul>
+        </Layout>
+    )
+}
